8264459d13c5ab125e54f5e85689cd0ec059d59c
Kerry Ulysse <kerryuly@gmail.com>
2019-02-18 20:16:28 -0500

    Pre-mobile compatibilty

---
+++ main.js
@@ -0,0 +1,39 @@
+// const left = document.querySelector(".left");
+// const right = document.querySelector(".right");
+// const container = document.querySelector(".container");
+
+// left.addEventListener("mouseenter", () => {
+//   container.classList.add("hover-left");
+// });
+
+// left.addEventListener("mouseleave", () => {
+//   container.classList.remove("hover-left");
+// });
+
+// right.addEventListener("mouseenter", () => {
+//   container.classList.add("hover-right");
+// });
+
+// right.addEventListener("mouseleave", () => {
+//   container.classList.remove("hover-right");
+// });
+
+var age = 23;
+// === compares variable and data type
+
+
+
+
+var students = ["Timmy", "Janessa", "Arun"]
+
+var naughtyList = [];
+naughtyList.psuh(index[0]);
+var index = naughtyList.indexOf("Timmy");
+
+if (index> -1){
+  naughtyList = naughtyList.splice(index, 1);
+}
+
+function area(lenght, width){
+  return lenght*width;
+}
